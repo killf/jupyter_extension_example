7c217814a399e1a5a1f756743341f3ba835c8f78
Ren Peng <renpeng@xiaoice.com>
2022-02-17 17:46:57 +0800

    update

--- jupyter_extension_example/__main__.py
+++
@@ -1,4 +0,0 @@
-from jupyter_extension_example.application import main
-
-if __name__ == "__main__":
-    main()
--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 import os
 
 from setuptools import setup
+from jupyter_packaging import create_cmdclass
 
 VERSION = "0.0.1"
+
+
+def get_data_files():
+    """Get the data files for the package."""
+    data_files = [
+        ("etc/jupyter/jupyter_server_config.d", "etc/jupyter/jupyter_server_config.d/", "*.json"),
+    ]
+
+    def add_data_files(path):
+        for (dir_path, dir_names, file_names) in os.walk(path):
+            if file_names:
+                paths = [(dir_path, dir_path, filename) for filename in file_names]
+                data_files.extend(paths)
+
+    # Add all static and templates folders.
+    add_data_files("jupyter_extension_example/static")
+    add_data_files("jupyter_extension_example/templates")
+    return data_files
+
+
+cmd_class = create_cmdclass(data_files_spec=get_data_files())
 
 setup_args = dict(
     name="jupyter_extension_example",
@@ -14,6 +36,12 @@ setup_args = dict(
         "jupyter_server",
     ],
     include_package_data=True,
+    cmdclass=cmd_class,
+    entry_points={
+        "console_scripts": [
+            "jupyter-extension-example = jupyter_extension_example.application:main",
+        ]
+    },
 )
 
 if __name__ == "__main__":
